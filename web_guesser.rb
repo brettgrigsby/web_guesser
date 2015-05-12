847ca576520cac795b5806fc1056c28d8ea72e30
brettgrigsby <gbrett@gmail.com>
2015-05-12 13:19:02 -0600

    color extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,22 +7,27 @@ SECRET_NUMBER = rand(101)
 def check_guess(guess_string)
   guess =  guess_string.to_i
   if guess > SECRET_NUMBER && (guess - SECRET_NUMBER) > 5
-    message = 'Way too high!'
+    mes = 'Way too high!'
+    color = 'red'
   elsif guess < SECRET_NUMBER && (SECRET_NUMBER - guess > 5)
-    message = 'Way too low!'
+    mes = 'Way too low!'
+    color = 'red'
   elsif guess > SECRET_NUMBER
-    message = 'too high!'
+    mes = 'too high!'
+    color = 'palevioletred'
   elsif guess < SECRET_NUMBER
-    message = 'too low!'
+    mes = 'too low!'
+    color = 'palevioletred'
   elsif guess == SECRET_NUMBER
-    message = 'You got it right!!'
+    mes = 'You got it right!!'
+    color = 'green'
   end
-  message
+  [mes, color]
 end
 
 get '/' do
   guess = params['guess']
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message.first, :color => message.last}
 end
 
